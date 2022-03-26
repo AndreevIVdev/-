8fb67b4dec666dd2ed94fd351d72cabada2117fc
Andreev Ilya <AndreevIV.dev@gmail.com>
2022-03-26 12:35:19 +0300

    style and api constants

---
+++ TestTaskMobileUP/Helpers/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  MobileUPTestTask
+//
+//  Created by Илья Андреев on 25.03.2022.
+//
+
+import UIKit
+
+enum Constants {
+    static let applicationID: String = "8115098"
+}
+
+enum Design {
+    static let padding: CGFloat = 24
+    static let bottomPadding: CGFloat = 50
+    static let buttonHeight: CGFloat = 55
+    static let cornerRadius: CGFloat = 5
+}
