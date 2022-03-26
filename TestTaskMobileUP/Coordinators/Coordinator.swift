9867403121b6fd3d133b4524afe1099c8d2ae7de
Andreev Ilya <AndreevIV.dev@gmail.com>
2022-03-26 16:57:25 +0300

    tiny combine code for coordinator logic

--- TestTaskMobileUP/Coordinators/Coordinator.swift
+++ TestTaskMobileUP/Coordinators/Coordinator.swift
@@ -7,11 +7,15 @@
 
 
 import UIKit
-
+import Combine
 
 final class Coordinator {
     
     private let window: UIWindow
+    private let navigationController: UINavigationController = .init()
+    private let authManager: AuthManager = .shared
+    
+    private var bindings: Set<AnyCancellable> = .init()
     
     init(
         window: UIWindow
@@ -20,16 +24,35 @@ final class Coordinator {
     }
     
     func start() {
-        window.rootViewController = AuthViewController(signIn: authButtonTapped)
+        authManager.$state
+            .sink { [weak self] state in
+                guard let self = self else { return }
+                switch state {
+                case .authorized:
+                    guard let token = self.authManager.getToken() else { return }
+                    self.navigationController.viewControllers = [
+                        GalleryViewController(
+                            token: token,
+                            signOut: self.handleSignOut
+                        )
+                    ]
+                    self.window.rootViewController = self.navigationController
+                case .unauthorized:
+                    self.window.rootViewController = AuthViewController(signIn: self.authButtonTapped)
+                case .error:
+                    print("error")
+                }
+            }
+            .store(in: &bindings)
     }
     
     private func authButtonTapped() {
-        let viewController: WebViewController = .init { [weak self] result in
+        guard let request = VKClient.login() else { return }
+        let viewController: WebViewController = .init(request: request) { [weak self] result in
             guard let self = self else { return }
             switch result {
-                
             case .success(let token):
-                print(token)
+                self.authManager.save(token)
             case .failure(let error):
                 print(error)
             }
@@ -38,5 +61,9 @@ final class Coordinator {
         DispatchQueue.main.async {
             self.window.rootViewController?.present(viewController, animated: true)
         }
+    }
+    
+    private func handleSignOut() {
+        authManager.logoutFromCurrentAccount()
     }
 }
